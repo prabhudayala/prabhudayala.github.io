0f71b68ba1d10cfc254a5e256e7cfa82b65bcb01
prabhudayala <prabhudayala@gmail.com>
2018-01-06 21:28:42 +0530

    Delete style.js

--- styles/style.js
+++
@@ -1,7 +0,0 @@
-html { 
-  background: url(https://s3.envato.com/files/83268071/Smoke3_590.jpg) no-repeat center center fixed; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
